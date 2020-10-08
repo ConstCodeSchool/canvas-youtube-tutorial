4a43e59478277fc2c15d04fa12f2474f4485e035
AlekseyDanchin <aleksey.danchin@gmail.com>
2020-10-08 15:45:41 +0300

    day_02

--- script.js
+++ script.js
@@ -1,16 +1,41 @@
 const canvas = document.querySelector("canvas");
 const context = canvas.getContext("2d");
 
-function resize() {
-	canvas.width = 500;
-	canvas.height = 500;
-}
+canvas.width = 500;
+canvas.height = 500;
 
-function clearCanvas(canvas) {
-	canvas.width = canvas.width;
-}
+// context.beginPath();
+// context.moveTo(0, 0);
+// context.lineTo(300, 400);
+// context.stroke();
+
+// context.beginPath();
+// context.moveTo(300, 400);
+// context.lineTo(500, 0);
+// context.stroke();
 
 context.beginPath();
-context.moveTo(0, 0);
-context.lineTo(500, 500);
+context.moveTo(100, 100);
+context.lineTo(300, 400);
+context.lineTo(450, 200);
+
+context.strokeStyle = "blue";
+context.lineWidth = 10;
+context.lineCap = "round";
+context.lineJoin = "bevel";
+
 context.stroke();
+
+// context.beginPath();
+// context.moveTo(0, 0);
+// context.lineTo(300, 400);
+// context.strokeStyle = "blue";
+// context.lineWidth = 10;
+// context.stroke();
+
+// context.beginPath();
+// context.moveTo(300, 400);
+// context.lineTo(500, 0);
+// context.strokeStyle = "red";
+// context.lineWidth = 2;
+// context.stroke();
